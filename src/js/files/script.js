ae53a6df31e31b8183b888465ebe19c8ef0551e6
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-08 16:22:15 +0300

    moved files to new folder derictory \ cleared files

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,18 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+if (document.querySelector("._filter-item")) {
+    const filterBox = document.querySelectorAll("._filter-item");
 
+    document.querySelector("._filter").addEventListener("click", (event) => {
+        if (event.target.tagName !== "LI") return false;
+        let filterClass = event.target.dataset["filter"];
+
+        filterBox.forEach((elem) => {
+            elem.classList.remove("_hide");
+            if (!elem.classList.contains(filterClass) && filterClass !== "all") {
+                elem.classList.add("_hide");
+            }
+        });
+    });
+}
