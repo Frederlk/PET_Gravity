334a26f77e29ae878cfc47e75405b28e4f96e1ec
Sergey Petrinich <petrinichshadow@gmail.com>
2022-08-09 16:50:18 +0300

    works v2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,10 +5,17 @@ import { flsModules } from "./modules.js";
 
 if (document.querySelector("._filter-item")) {
     const filterBox = document.querySelectorAll("._filter-item");
-
+    const links = document.querySelectorAll(".works__link");
     document.querySelector("._filter").addEventListener("click", (event) => {
         if (event.target.tagName !== "LI") return false;
         let filterClass = event.target.dataset["filter"];
+
+        links.forEach((elem) => {
+            elem.classList.remove("_active");
+            if (elem.innerHTML.toLowerCase() === filterClass.toLowerCase()) {
+                elem.classList.add("_active");
+            }
+        });
 
         filterBox.forEach((elem) => {
             elem.classList.remove("_hide");
